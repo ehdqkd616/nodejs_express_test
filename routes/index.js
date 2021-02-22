6934233165742b53a0b1c54cabfa2d35a02ec0f9
Rudy <my4my4@gmail.com>
2021-02-22 18:11:37 +0900

    MSSQL Insert Success

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const { Connection, Request } = require("tedious");
+const TYPES = require('tedious').TYPES;
 
 // Create connection to database
 var config = {
   authentication: {
     options: {
-      userName: 'user', // userName
-      password: 'user'  // password
+      userName: 'yw', // userName
+      password: 'yw'  // password
     },
     type: 'default'
   },
-  server: '127.0.0.1',  // server 주소  
+  server: '192.168.10.225',  // server 주소  
   options: {
-    database: 'express',  // database name
-    encrypt: true
-  }
+    database: 'YW_Test',  // database name
+    encrypt: false
+  } 
 }; 
 
 const connection = new Connection(config);
@@ -28,7 +29,7 @@ connection.on('connect', function (err) {
     console.error(err);
   } else {
     console.log("Connected");
-    queryDatabase();
+    // queryDatabase();
   }
 });  
 
@@ -38,7 +39,7 @@ function queryDatabase() {
     console.log("Reading rows from the Table...");
 
     // Read all rows from table
-    const request = new Request("select * from TEST;", function (err) {  
+    const request = new Request("select * from TEST;", function (err, rowCount) {  
       if (err, rowCount) {  
         console.log(err);  
       } else {
@@ -63,41 +64,167 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* GET home page. */
-router.get('/get', function(req, res, next) {
+
+router.get('/get_sql', function(req, res, next) {
   
   var queryString = req.query;
   var queryString_json = JSON.stringify(queryString);
-
   insert(queryString_json);
-  res.end('Get Insert Success')
+
+  res.end('Get_SQL Insert');
   // res.send(queryString);
   // res.render('index', { title: `Get Parameter : ${queryString} `});
 });
 
-/* GET home page. */
-router.post('/post', function(req, res, next) {
+
+router.get('/get_procedure', function(req, res, next) {
+  
+  var queryString = req.query;
+  var queryString_json = JSON.stringify(queryString);
+  insert_procedure(queryString_json);
+
+  res.end('Get_PROCEDURE Insert');
+  // res.send(queryString);
+  // res.render('index', { title: `Get Parameter : ${queryString} `});
+});
+
+
+router.post('/post_sql', function(req, res, next) {
 
   var body = req.body;
   var body_json = JSON.stringify(body);
-
   insert(body_json);
-  res.end('Post Insert Success');
+
+  res.end('Post_SQL Insert');
   // res.send(body);
   // res.render('index', { title: `Post Parameter : ${body} `});
 });
 
-function insert(data) {  
+router.post('/post_procedure', function(req, res, next) { // 프로시저 이용
+
+  var body = req.body;
+  var body_json = JSON.stringify(body);
+  insert_procedure(body_json);
   
-  var sql = `INSERT INTO TEST(JSON_DATA, TIME) VALUES(${data}, GETDATE())`;
+  res.end('Post_PROCEDURE Insert');
+  // res.send(body);
+  // res.render('index', { title: `Post Parameter : ${body} `});
+});
+
+function insert(data, result) {
+  
+  var sql = `INSERT INTO TEST(JSON_DATA, INSERT_TIME) VALUES('${data}', GETDATE())`;
+  var result = "";
+
+  console.log(sql); // sql syntax 에러 발생시 sql문을 log로 출력해본다.!!!!!!!!
   
   request = new Request(sql, function(err) {  
   
-  if (err) {  
-      console.log(err);}  
+    if (err) {  
+      console.log(err);
+      result = 'Error Occurred!!!';
+    }else{
+      console.log('Insert 성공');
+      result = 'Insert Success!';
+    }
   });
   
-  connection.execSql(request);  
+  connection.execSql(request);
+
+} 
+
+function insert_procedure(data) {  
+  
+  var sql = 'INSERT_PROCEDURE';
+  var result = "";
+  
+  // console.log(sql); // sql syntax 에러 발생시 sql문을 log로 출력해본다.!!!!!!!!
+  
+  request = new Request(sql, function(err, rowCount, rows) {  
+    
+    if (err) { // 에러 발생 시
+      console.log("request err : " + err);
+      console.log('-------------------------------------');
+      result = 'Error Occurred!!!';
+    }else{  // 요청 성공 시
+      console.log("request rowCount : " + rowCount);
+      console.log("request rows : " + rows);
+      console.log('-------------------------------------');
+      result = 'Insert Success!';
+    }
+  });
+  
+  request.addParameter('json_data', TYPES.NVarChar, data);
+  // request.addOutputParameter('id', TYPES.Int);
+  
+  request.on('columnMetadata', function (columns) {
+    columns.forEach(function (column) {
+      console.log("event olumnMetadata");
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+    console.log('-------------------------------------');
+  });   
+  
+  request.on('prepared', function () {
+    console.log('event prepared');
+    console.log('-------------------------------------');
+  });
+  
+  request.on('error', function (err) {
+    console.log('error : '+err);
+    console.log('-------------------------------------');
+  });
+  
+  request.on('requestCompleted', function () {
+    console.log('event requestCompleted');
+    console.log('-------------------------------------');
+  });
+  
+  request.on('row', function (columns) {
+    columns.forEach(function (column) {
+      console.log("event row");
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+    console.log('-------------------------------------');
+  });
+  
+  request.on('done', function (rowCount, more, rows) {
+    console.log('event requestCompleted');
+    console.log('rowCount : ' + rowCount);
+    console.log('more : ' + more);
+    console.log('rows : ' + rows);
+    console.log('-------------------------------------');
+  }); 
+  
+  request.on('doneInProc', function (rowCount, more, rows) { 
+    console.log('event doneInProc');
+    console.log('rowCount : ' + rowCount);
+    console.log('more : ' + more);
+    console.log('rows : ' + rows);
+    console.log('-------------------------------------');
+  });
+  
+  request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+    console.log('event doneProc');
+    console.log('rowCount : ' + rowCount);
+    console.log('more : ' + more);
+    console.log('returnStatus : ' + returnStatus);
+    console.log('rows : ' + rows);
+    console.log('-------------------------------------');
+  });
+  
+  request.on('returnValue', function (parameterName, value, metadata) {
+    console.log('event returnValue');
+    console.log('parameterName : ' + parameterName);
+    console.log('value : ' + value);
+    console.log('metadata : ' + metadata);
+    console.log('-------------------------------------');
+  });
+  
+  connection.callProcedure(request);
+  
+  console.log('result : '+result);
+  return result;
 }  
 
 module.exports = router;
